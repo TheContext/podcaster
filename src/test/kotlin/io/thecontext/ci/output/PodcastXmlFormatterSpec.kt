c434325dd2c29a03a60ade37c5406764a2d50a1f
Artur Dryomov <artur.dryomov@gmail.com>
2018-12-27 09:58:44 +0300

    Add content namespace to podcast XML.

--- src/test/kotlin/io/thecontext/ci/output/PodcastXmlFormatterSpec.kt
+++ src/test/kotlin/io/thecontext/ci/output/PodcastXmlFormatterSpec.kt
@@ -22,7 +22,7 @@ class PodcastXmlFormatterSpec {
             it("formats") {
                 val expected = """
                     <?xml version="1.0" encoding="utf-8"?>
-                    <rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
+                    <rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:content="http://purl.org/rss/1.0/modules/content/">
                       <channel>
                         <title>${podcast.title}</title>
                         <description>${podcast.description}</description>
