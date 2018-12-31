b53493222102ac36bf1332c9060e7c0bdfdee8d1
Artur Dryomov <artur.dryomov@gmail.com>
2018-12-31 17:57:08 +0300

    Remove show notes generation.

--- src/main/kotlin/io/thecontext/ci/output/OutputWriter.kt
+++ src/main/kotlin/io/thecontext/ci/output/OutputWriter.kt
@@ -2,8 +2,7 @@ package io.thecontext.ci.output
 
 import io.reactivex.Scheduler
 import io.reactivex.Single
-import io.reactivex.functions.Function3
-import io.thecontext.ci.output.feed.FeedEpisodeRenderer
+import io.reactivex.functions.BiFunction
 import io.thecontext.ci.output.feed.FeedRenderer
 import io.thecontext.ci.output.website.WebsiteRenderer
 import io.thecontext.ci.value.Episode
@@ -17,33 +16,16 @@ interface OutputWriter {
         const val FEED = "podcast.rss"
     }
 
-    fun write(showNotesDirectory: File, rssFeedDirectory: File, websiteDirectory: File, podcast: Podcast, episodes: List<Episode>, people: List<Person>): Single<Unit>
+    fun write(rssFeedDirectory: File, websiteDirectory: File, podcast: Podcast, episodes: List<Episode>, people: List<Person>): Single<Unit>
 
     class Impl(
             private val feedRenderer: FeedRenderer,
-            private val feedEpisodeRenderer: FeedEpisodeRenderer,
             private val websiteRenderer: WebsiteRenderer,
             private val textWriter: TextWriter,
             private val ioScheduler: Scheduler
     ) : OutputWriter {
 
-        override fun write(showNotesDirectory: File, rssFeedDirectory: File, websiteDirectory: File, podcast: Podcast, episodes: List<Episode>, people: List<Person>): Single<Unit> {
-            val notes = Single
-                    .merge(episodes.map { episode -> feedEpisodeRenderer.render(podcast, episode, people).map { episode to it } })
-                    .toList()
-                    .flatMap {
-                        val operations = it.map { (episode, episodeMarkdown) ->
-                            Single.fromCallable {
-                                showNotesDirectory.mkdirs()
-
-                                textWriter.write(File(showNotesDirectory, "${episode.slug}.md"), episodeMarkdown)
-                            }
-                        }
-
-                        Single.merge(operations).toList()
-                    }
-                    .map { Unit }
-
+        override fun write(rssFeedDirectory: File, websiteDirectory: File, podcast: Podcast, episodes: List<Episode>, people: List<Person>): Single<Unit> {
             val feed = feedRenderer.render(podcast, episodes, people)
                     .flatMap { podcastXml ->
                         Single.fromCallable {
@@ -69,7 +51,7 @@ interface OutputWriter {
                     }
                     .map { Unit }
 
-            return Single.zip(notes, feed, website, Function3<Unit, Unit, Unit, Unit> { _, _, _ -> Unit }).subscribeOn(ioScheduler)
+            return Single.zip(feed, website, BiFunction<Unit, Unit, Unit> { _, _ -> Unit }).subscribeOn(ioScheduler)
         }
     }
 }
