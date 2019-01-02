f0f240a5021b3296649ba66b5a932d451d3f4da5
Artur Dryomov <artur.dryomov@gmail.com>
2019-01-02 18:29:16 +0300

    Change people order to be alphabetic.

--- src/main/kotlin/io/thecontext/ci/output/MarkdownEpisodeRenderer.kt
+++ src/main/kotlin/io/thecontext/ci/output/MarkdownEpisodeRenderer.kt
@@ -17,8 +17,12 @@ interface MarkdownEpisodeRenderer {
 
         override fun render(template: TemplateRenderer.Template, episode: Episode, people: List<Person>) = Single
                 .fromCallable {
-                    val guestIds = episode.people.guestIds.orEmpty()
-                    val hostIds = episode.people.hostIds
+                    val guests = episode.people.guestIds.orEmpty()
+                            .map { people.find(it) }
+                            .sortedBy { it.name }
+                    val hosts = episode.people.hostIds
+                            .map { people.find(it) }
+                            .sortedBy { it.name }
 
                     val contents = mapOf(
                             "number" to episode.number,
@@ -27,10 +31,10 @@ interface MarkdownEpisodeRenderer {
                             "title" to episode.title,
                             "description" to episode.description,
                             "notes" to episode.notesMarkdown,
-                            "guests_available" to guestIds.isNotEmpty(),
-                            "guests" to guestIds.map { people.find(it) }.map { mapOf("guest" to formatPerson(it)) },
-                            "hosts_available" to hostIds.isNotEmpty(),
-                            "hosts" to hostIds.map { people.find(it) }.map { mapOf("host" to formatPerson(it)) },
+                            "guests_available" to guests.isNotEmpty(),
+                            "guests" to guests.map { mapOf("guest" to formatPerson(it)) },
+                            "hosts_available" to hosts.isNotEmpty(),
+                            "hosts" to hosts.map { mapOf("host" to formatPerson(it)) },
                             "discussion_url" to episode.discussionUrl,
                             "file_url" to episode.file.url
                     )
