c32cdd4c9ee190b25c6a494fe66e4ce30a621646
Artur Dryomov <artur.dryomov@gmail.com>
2019-01-05 11:46:28 +0300

    Add result codes.

--- src/main/kotlin/io/thecontext/ci/Main.kt
+++ src/main/kotlin/io/thecontext/ci/Main.kt
@@ -3,6 +3,11 @@ package io.thecontext.ci
 import io.thecontext.ci.context.Context
 import java.io.File
 
+enum class ResultCode(val value: Int) {
+    Success(0),
+    Failure(1),
+}
+
 fun main(args: Array<String>) {
     val arguments = Arguments.from(args)
     val runner = Runner.Impl(Context.Impl())
@@ -13,8 +18,20 @@ fun main(args: Array<String>) {
 
     val result = runner.run(inputDirectory, outputFeedFile, outputWebsiteDirectory).blockingGet()
 
-    when (result) {
-        is Runner.Result.Success -> println(":: Success!")
-        is Runner.Result.Failure -> listOf(":: Failure.", result.message).forEach { println(it) }
+    val resultCode = when (result) {
+        is Runner.Result.Success -> {
+            println(":: Success!")
+
+            ResultCode.Success
+        }
+
+        is Runner.Result.Failure -> {
+            println(":: Failure.")
+            println(result.message)
+
+            ResultCode.Failure
+        }
     }
+
+    System.exit(resultCode.value)
 }
